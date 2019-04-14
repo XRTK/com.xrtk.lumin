8c9bd0a5c69e2932a61a89ab4cb1ffe420909d51
StephenHodgson <hodgson.designs@gmail.com>
2019-04-14 15:01:43 -0700

    Initial commit of Lumin platform components

---
+++ XRTK.Lumin/Assets/XRTK.Lumin/Profiles/LuminMotionControllerMappingProfile.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using XRTK.Definitions.Devices;
+using XRTK.Definitions.Utilities;
+using XRTK.Lumin.Controllers;
+using XRTK.Providers.Controllers;
+
+namespace XRTK.Lumin.Profiles
+{
+    [CreateAssetMenu(menuName = "Mixed Reality Toolkit/Input System/Controller Mappings/Lumin Controller Mapping Profile", fileName = "LuminControllerMappingProfile")]
+    public class LuminMotionControllerMappingProfile : BaseMixedRealityControllerMappingProfile
+    {
+        /// <inheritdoc />
+        public override SupportedControllerType ControllerType => SupportedControllerType.Lumin;
+
+        /// <inheritdoc />
+        public override string TexturePath => $"{base.TexturePath}MotionController";
+
+        protected override void Awake()
+        {
+            if (!HasSetupDefaults)
+            {
+                ControllerMappings = new[]
+                {
+                    new MixedRealityControllerMapping("Lumin Motion Controller Left", typeof(LuminController), Handedness.Left),
+                    new MixedRealityControllerMapping("Lumin Motion Controller Right", typeof(LuminController), Handedness.Right),
+                };
+            }
+
+            base.Awake();
+        }
+    }
+}
