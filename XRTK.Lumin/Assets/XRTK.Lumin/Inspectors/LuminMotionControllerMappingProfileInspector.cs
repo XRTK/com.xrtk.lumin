8c22eb31c3393e12dbcbc616c45537bfdb5630b5
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2019-04-26 18:43:33 -0700

    Version 0.1.0 (#10)
    * updated project to 2019.1.0
    * Initial commit of Lumin platform components
    * updated Lumin asemdef
    * fixed readme and updated package dependencies
    * fixed controller texture path
    * updated editor and packages
    * updated package dependencies
    * updated project settings

---
+++ XRTK.Lumin/Assets/XRTK.Lumin/Inspectors/LuminMotionControllerMappingProfileInspector.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+using XRTK.Inspectors.Profiles;
+using XRTK.Lumin.Profiles;
+
+namespace XRTK.Lumin.Inspectors
+{
+    [CustomEditor(typeof(LuminMotionControllerMappingProfile))]
+    public class LuminMotionControllerMappingProfileInspector : BaseMixedRealityControllerMappingProfileInspector
+    {
+    }
+}
