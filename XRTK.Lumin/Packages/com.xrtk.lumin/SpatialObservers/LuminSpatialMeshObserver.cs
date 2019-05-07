db42b4b858a18083d15cffaab637d2562ff7260d
StephenHodgson <hodgson.designs@gmail.com>
2019-05-07 08:19:36 -0700

    added initial files for spatial awareness

---
+++ XRTK.Lumin/Packages/com.xrtk.lumin/SpatialObservers/LuminSpatialMeshObserver.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Providers.SpatialObservers;
+
+namespace XRTK.Lumin.SpatialObservers
+{
+    /// <summary>
+    /// Lumin Spatial Mesh Observer
+    /// </summary>
+    public class LuminSpatialMeshObserver : BaseMixedRealitySpatialMeshObserver
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="priority"></param>
+        /// <param name="profile"></param>
+        public LuminSpatialMeshObserver(string name, uint priority, BaseMixedRealitySpatialMeshObserverProfile profile) : base(name, priority, profile)
+        {
+        }
+    }
+}
