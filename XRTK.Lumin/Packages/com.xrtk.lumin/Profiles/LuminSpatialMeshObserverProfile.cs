f6e2804fcb503d74928fec508782bd9c87e541da
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2019-05-09 18:21:36 -0700

    Dev spatial awareness (#20)
    * added initial files for spatial awareness
    
    * Implemented LuminSpatialObserver
    
    * blocked out platform support
    
    * removed a few more base profile references

---
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Profiles/LuminSpatialMeshObserverProfile.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using XRTK.Definitions.Utilities;
+using XRTK.Providers.SpatialObservers;
+
+namespace XRTK.Lumin.Profiles
+{
+    [CreateAssetMenu(menuName = "Mixed Reality Toolkit/Spatial Observers/Lumin Spatial Mesh Data Provider Profile", fileName = "LuminMeshObserverProfile", order = (int)CreateProfileMenuItemIndices.SpatialAwarenessDataProviders)]
+    public class LuminSpatialMeshObserverProfile : BaseMixedRealitySpatialMeshObserverProfile { }
+}
