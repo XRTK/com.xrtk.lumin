35cb7f9a6b5d4c628fa328846f9244abb7bbd695
Dino Fejzagic <dino.f@live.de>
2019-08-15 23:27:53 +0200

    Fix base class

--- XRTK.Lumin/Packages/com.xrtk.lumin/Controllers/LuminHandControllerDataProvider.cs
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Controllers/LuminHandControllerDataProvider.cs
@@ -1,21 +1,17 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using XRTK.Definitions.Controllers;
 using XRTK.Interfaces.Providers.Controllers;
-using XRTK.Services;
+using XRTK.Providers.Controllers;
 
 namespace XRTK.Lumin.Controllers
 {
-    public class LuminHandControllerDataProvider : BaseDataProvider, IMixedRealityPlatformHandControllerDataProvider
+    public class LuminHandControllerDataProvider : BaseControllerDataProvider, IMixedRealityPlatformHandControllerDataProvider
     {
-        public LuminHandControllerDataProvider(string name, uint priority)
-            : base(name, priority)
+        public LuminHandControllerDataProvider(string name, uint priority, BaseMixedRealityControllerDataProviderProfile profile)
+            : base(name, priority, profile)
         {
-        }
-
-        public IMixedRealityController[] GetActiveControllers()
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
