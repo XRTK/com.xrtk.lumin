e95cbf6acde4a7978bced521fe9c9eb00a49506d
Dino Fejzagic <dino.f@live.de>
2019-08-15 20:27:19 +0200

    Add Lumin hand controller data provider

---
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Controllers/LuminHandControllerDataProvider.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.Interfaces.Providers.Controllers;
+using XRTK.Services;
+
+namespace XRTK.Lumin.Controllers
+{
+    public class LuminHandControllerDataProvider : BaseDataProvider, IMixedRealityPlatformHandControllerDataProvider
+    {
+        public LuminHandControllerDataProvider(string name, uint priority)
+            : base(name, priority)
+        {
+        }
+
+        public IMixedRealityController[] GetActiveControllers()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
