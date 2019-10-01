bdcf165fc263432b6d5a2910831ccad22167ce08
Dino Fejzagic <dino.f@live.de>
2019-10-01 17:20:22 +0200

    Remove obsolete lumin platform hand controller data provider

--- XRTK.Lumin/Packages/com.xrtk.lumin/Controllers/LuminHandControllerDataProvider.cs
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Controllers/LuminHandControllerDataProvider.cs
@@ -1,20 +1,14 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using XRTK.Definitions.Controllers;
-using XRTK.Interfaces.Providers.Controllers;
-using XRTK.Providers.Controllers;
+using XRTK.Definitions.Controllers.Hands;
+using XRTK.Providers.Controllers.Hands;
 
 namespace XRTK.Lumin.Controllers
 {
-    public class LuminHandControllerDataProvider : BaseControllerDataProvider, IMixedRealityPlatformHandControllerDataProvider
+    public class LuminHandControllerDataProvider : BaseHandControllerDataProvider
     {
-        /// <inheritdoc />
-        public event HandDataUpdate OnHandDataUpdate;
-
-        public LuminHandControllerDataProvider(string name, uint priority, BaseMixedRealityControllerDataProviderProfile profile)
-            : base(name, priority, profile)
-        {
-        }
+        public LuminHandControllerDataProvider(string name, uint priority, HandControllerDataProviderProfile profile)
+            : base(name, priority, profile) { }
     }
 }
