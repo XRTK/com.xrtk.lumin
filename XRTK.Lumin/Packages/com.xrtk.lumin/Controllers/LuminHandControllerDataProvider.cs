00dd045e218262867995427d672de2cc0353528f
Dino Fejzagic <dino.f@live.de>
2019-10-31 17:33:12 +0100

    Remove need for hand controller data provider profile

--- XRTK.Lumin/Packages/com.xrtk.lumin/Controllers/LuminHandControllerDataProvider.cs
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Controllers/LuminHandControllerDataProvider.cs
@@ -1,14 +1,14 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using XRTK.Definitions.Controllers.Hands;
+using XRTK.Definitions.Controllers;
 using XRTK.Providers.Controllers.Hands;
 
 namespace XRTK.Lumin.Controllers
 {
-    public class LuminHandControllerDataProvider : BaseHandControllerDataProvider
+    public class LuminHandControllerDataProvider : BaseHandControllerDataProvider<LuminHandController>
     {
-        public LuminHandControllerDataProvider(string name, uint priority, HandControllerDataProviderProfile profile)
+        public LuminHandControllerDataProvider(string name, uint priority, BaseMixedRealityControllerDataProviderProfile profile)
             : base(name, priority, profile) { }
     }
 }
