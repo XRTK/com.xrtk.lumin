7e483d448de7934fb9c69f00aedc0b86f915579c
Dino Fejzagic <dino.f@live.de>
2019-11-25 10:36:59 +0100

    Implement Lumin data provider profile inspector

---
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Inspectors/LuminHandControllerDataProviderProfileInspector.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+using UnityEngine;
+using XRTK.Inspectors.Profiles;
+using XRTK.Inspectors.Utilities;
+using XRTK.Lumin.Profiles;
+
+namespace XRTK.Lumin.Inspectors
+{
+    [CustomEditor(typeof(LuminHandControllerDataProviderProfile))]
+    public class LuminHandControllerDataProviderProfileInspector : BaseMixedRealityProfileInspector
+    {
+        private SerializedProperty keyPointFilterLevel;
+        private SerializedProperty poseFilterLevel;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            keyPointFilterLevel = serializedObject.FindProperty("keyPointFilterLevel");
+            poseFilterLevel = serializedObject.FindProperty("poseFilterLevel");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            MixedRealityInspectorUtility.RenderMixedRealityToolkitLogo();
+
+            if (thisProfile.ParentProfile != null &&
+                GUILayout.Button("Back to Configuration Profile"))
+            {
+                Selection.activeObject = thisProfile.ParentProfile;
+            }
+
+            EditorGUILayout.Space();
+            thisProfile.CheckProfileLock();
+
+            serializedObject.Update();
+
+            EditorGUILayout.BeginVertical("Label");
+
+            EditorGUILayout.PropertyField(keyPointFilterLevel);
+            EditorGUILayout.PropertyField(poseFilterLevel);
+
+            EditorGUILayout.EndVertical();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
