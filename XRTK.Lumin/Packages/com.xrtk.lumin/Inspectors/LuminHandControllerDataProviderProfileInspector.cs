c31f3aea928474d5a344316f014a3d788ec3b90f
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-05 16:29:46 -0400

    updated lumin hand controller provider profile

--- XRTK.Lumin/Packages/com.xrtk.lumin/Inspectors/LuminHandControllerDataProviderProfileInspector.cs
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Inspectors/LuminHandControllerDataProviderProfileInspector.cs
@@ -1,24 +1,33 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using UnityEditor;
+using UnityEngine;
 using XRTK.Inspectors.Profiles.InputSystem.Controllers;
 using XRTK.Lumin.Profiles;
+
+using UnityEngine.XR.MagicLeap;
 
 namespace XRTK.Lumin.Inspectors
 {
     [CustomEditor(typeof(LuminHandControllerDataProviderProfile))]
     public class LuminHandControllerDataProviderProfileInspector : BaseMixedRealityHandControllerDataProviderProfileInspector
     {
-        private SerializedProperty keyPointFilterLevel;
         private SerializedProperty poseFilterLevel;
+        private SerializedProperty keyPointFilterLevel;
+
+        private GUIContent keyPointContent;
+        private GUIContent poseFilterContent;
 
         protected override void OnEnable()
         {
             base.OnEnable();
 
             keyPointFilterLevel = serializedObject.FindProperty(nameof(keyPointFilterLevel));
+            keyPointContent = new GUIContent(keyPointFilterLevel.displayName, keyPointFilterLevel.tooltip);
             poseFilterLevel = serializedObject.FindProperty(nameof(poseFilterLevel));
+            poseFilterContent = new GUIContent(poseFilterLevel.displayName, poseFilterLevel.tooltip);
         }
 
         public override void OnInspectorGUI()
@@ -32,8 +41,8 @@ namespace XRTK.Lumin.Inspectors
 
             EditorGUILayout.Space();
             EditorGUILayout.LabelField("Lumin Hand Settings");
-            EditorGUILayout.PropertyField(keyPointFilterLevel);
-            EditorGUILayout.PropertyField(poseFilterLevel);
+            keyPointFilterLevel.intValue = (int)(MLKeyPointFilterLevel)EditorGUILayout.EnumPopup(keyPointContent, (MLKeyPointFilterLevel)keyPointFilterLevel.intValue);
+            poseFilterLevel.intValue = (int)(MLPoseFilterLevel)EditorGUILayout.EnumPopup(poseFilterContent, (MLPoseFilterLevel)poseFilterLevel.intValue);
 
             serializedObject.ApplyModifiedProperties();
         }
