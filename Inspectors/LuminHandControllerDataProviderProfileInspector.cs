8b480ba0b51031ca66654dd485b0694c2ed9e56d
Dino Fejzagic <dino.f@live.de>
2020-04-07 17:59:53 +0200

    Feature - Hand Controllers + Simulation - LUMIN (#49)
    * Add Lumin hand controller data provider
    
    * Fix base class
    
    * Implement hand data provider interface
    
    * Remove obsolete lumin platform hand controller data provider
    
    * Create controller stub
    
    * Remove need for hand controller data provider profile
    
    * Revert Lumin speciifc hand controller
    
    * Implement Lumin data provider profile
    
    * Implement Lumin data provider profile inspector
    
    * Add XRTK assembly reference to fix inspector missing assembly issue
    
    * Fix deprecated Lumin controller API
    
    * Implement Lumin hands init / deinit
    
    * Implement hand data conversion
    
    * Implement is tracking enabled flag for Lumin
    
    * Add comment for missing rotation
    
    * Fix Lumin dependent compilation
    
    * Use LateUpdate for hand update instead of Update
    
    * Fix Unity serialization error when selecting Lumin profile asset when not targeting Lumin
    
    * Implement Lumin profile lock using new Utility
    
    * Update profile platform utility usage
    
    * Resolve merge issue
    
    * Fix platform flag for Lumin inspector
    
    * Set file icons
    
    * Create Lumin hand controller and mapping profile + inspector
    
    * Add required RefreshActiveControllers override to build
    
    * Removed base hand data provider
    
    * Move hand controller detection into existin lcdp
    
    * Adjust Lumin to recent changes
    
    * Bring back lumin hand controller data provider profile
    
    * Move LuminControllerDataProvider constructor to top
    
    * Reset Lumin data provider controller
    
    * Update hand controller data provider to fit recent changes
    
    * Remove hand tracking enabled setting (disable by removing data provider)
    
    * Update convert to set all missing joints zero fo rnow
    
    * Introduce shared hand profile configuraiton
    
    * Introduce base hand controller data provide to consume shared profile
    
    * Add label to Lumin specific in inspector
    
    * Introduce HandMeshingEnabled profile setting
    
    * Introduce "Hand Tracking System Profile"
    
    * Remove package dependency to legacy magic leap package
    
    * update lumin hand controller data provider (#51)
    
    * update lumin hand controller data provider
    
    * updated base controller data provider profile namespace
    
    * fixed compiler errors from platform changes
    
    * Fusion Lumin controller dp profiles
    
    * updated inspector
    
    * bumped minor version
    
    * Remove hand ray type setting
    
    * Change requests/hand tracking (#54)
    
    * updated references
    
    * updated references and cleaned up a bit
    
    * moved LuminHandDataConverter to Lumin.Utilities
    
    * sorted usings
    
    * fixed hand data provider inspector
    
    * Restore LuminHandControllerDataProvider
    
    * Restore lumin package dependency
    
    * fixed compiler errors
    
    * updated lumin hand controller provider profile
    
    * added a check if configuration was applied
    
    misc formatting
    
    * fix compiler errors
    
    * remove hand calls from controller data provider
    
    * Fixed lumin hand controller support (#55)
    
    * Fixed lumin hand controller support
    
    updated palm pose
    
    * only remove from registry if flag set
    
    Co-authored-by: Stephen Hodgson <hodgson.designs@gmail.com>
    Co-authored-by: Stephen Hodgson <StephenHodgson@users.noreply.github.com>

---
+++ Inspectors/LuminHandControllerDataProviderProfileInspector.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#if PLATFORM_LUMIN
+
+using UnityEditor;
+using UnityEngine;
+using XRTK.Inspectors.Profiles.InputSystem.Controllers;
+using XRTK.Lumin.Profiles;
+using UnityEngine.XR.MagicLeap;
+
+namespace XRTK.Lumin.Inspectors
+{
+    [CustomEditor(typeof(LuminHandControllerDataProviderProfile))]
+    public class LuminHandControllerDataProviderProfileInspector : BaseMixedRealityHandControllerDataProviderProfileInspector
+    {
+        private SerializedProperty poseFilterLevel;
+        private SerializedProperty keyPointFilterLevel;
+
+        private GUIContent keyPointContent;
+        private GUIContent poseFilterContent;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            keyPointFilterLevel = serializedObject.FindProperty(nameof(keyPointFilterLevel));
+            keyPointContent = new GUIContent(keyPointFilterLevel.displayName, keyPointFilterLevel.tooltip);
+            poseFilterLevel = serializedObject.FindProperty(nameof(poseFilterLevel));
+            poseFilterContent = new GUIContent(poseFilterLevel.displayName, poseFilterLevel.tooltip);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            RenderHeader();
+
+            EditorGUILayout.LabelField("Lumin Hand Controller Data Provider Settings", EditorStyles.boldLabel);
+
+            base.OnInspectorGUI();
+            serializedObject.Update();
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Lumin Hand Settings");
+
+            keyPointFilterLevel.intValue = (int)(MLKeyPointFilterLevel)EditorGUILayout.EnumPopup(keyPointContent, (MLKeyPointFilterLevel)keyPointFilterLevel.intValue);
+            poseFilterLevel.intValue = (int)(MLPoseFilterLevel)EditorGUILayout.EnumPopup(poseFilterContent, (MLPoseFilterLevel)poseFilterLevel.intValue);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+#endif // PLATFORM_LUMIN
