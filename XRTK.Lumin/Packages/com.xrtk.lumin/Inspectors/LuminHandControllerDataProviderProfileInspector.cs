6ee1a551a649f73363ef2ab393abd62ea6c16e42
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-11 20:28:23 -0400

    updated hand inspector

--- XRTK.Lumin/Packages/com.xrtk.lumin/Inspectors/LuminHandControllerDataProviderProfileInspector.cs
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Inspectors/LuminHandControllerDataProviderProfileInspector.cs
@@ -1,13 +1,15 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-#if PLATFORM_LUMIN
 
 using UnityEditor;
 using UnityEngine;
 using XRTK.Inspectors.Profiles.InputSystem.Controllers;
 using XRTK.Lumin.Profiles;
+
+#if PLATFORM_LUMIN
 using UnityEngine.XR.MagicLeap;
+#endif // PLATFORM_LUMIN
 
 namespace XRTK.Lumin.Inspectors
 {
@@ -36,14 +38,16 @@ namespace XRTK.Lumin.Inspectors
 
             serializedObject.Update();
 
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Lumin Hand Settings");
-
+            EditorGUILayout.LabelField("Oculus Hand Settings", EditorStyles.boldLabel);
+#if PLATFORM_LUMIN
             keyPointFilterLevel.intValue = (int)(MLKeyPointFilterLevel)EditorGUILayout.EnumPopup(keyPointContent, (MLKeyPointFilterLevel)keyPointFilterLevel.intValue);
             poseFilterLevel.intValue = (int)(MLPoseFilterLevel)EditorGUILayout.EnumPopup(poseFilterContent, (MLPoseFilterLevel)poseFilterLevel.intValue);
+#else
+            EditorGUILayout.PropertyField(keyPointFilterLevel);
+            EditorGUILayout.PropertyField(poseFilterLevel);
+#endif // PLATFORM_LUMIN
 
             serializedObject.ApplyModifiedProperties();
         }
     }
 }
-#endif // PLATFORM_LUMIN
