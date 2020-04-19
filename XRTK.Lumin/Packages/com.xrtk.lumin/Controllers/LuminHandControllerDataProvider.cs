55a74adff173d4e9de7f5a114d56953d4fcce5bc
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-19 12:21:50 -0400

    Updated constructors for data providers (#65)

--- XRTK.Lumin/Packages/com.xrtk.lumin/Controllers/LuminHandControllerDataProvider.cs
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Controllers/LuminHandControllerDataProvider.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using XRTK.Interfaces.InputSystem;
 using XRTK.Lumin.Profiles;
 using XRTK.Providers.Controllers.Hands;
 
@@ -22,14 +23,9 @@ namespace XRTK.Lumin.Controllers
 {
     public class LuminHandControllerDataProvider : BaseHandControllerDataProvider
     {
-        /// <summary>
-        /// Creates a new instance of the data provider.
-        /// </summary>
-        /// <param name="name">Name of the data provider as assigned in the configuration profile.</param>
-        /// <param name="priority">Data provider priority controls the order in the service registry.</param>
-        /// <param name="profile">Hand controller data provider profile assigned to the provider instance in the configuration inspector.</param>
-        public LuminHandControllerDataProvider(string name, uint priority, LuminHandControllerDataProviderProfile profile)
-            : base(name, priority, profile)
+        /// <inheritdoc />
+        public LuminHandControllerDataProvider(string name, uint priority, LuminHandControllerDataProviderProfile profile, IMixedRealityInputSystem parentService)
+            : base(name, priority, profile, parentService)
         {
 #if PLATFORM_LUMIN
             keyPointFilterLevel = (MLKeyPointFilterLevel)profile.KeyPointFilterLevel;
