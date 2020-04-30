83c77f3172b08328f8734e73e1a7c7db826d54a2
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-30 08:12:20 -0400

    Added package installer

---
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Editor/LuminPackageInstaller.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.IO;
+using UnityEditor;
+using XRTK.Editor;
+using XRTK.Extensions;
+using XRTK.Utilities.Editor;
+
+
+namespace XRTK.Lumin.Editor
+{
+    [InitializeOnLoad]
+    internal static class LuminPackageInstaller
+    {
+        private static readonly string DefaultPath = $"{MixedRealityPreferences.ProfileGenerationPath}Lumin\\";
+        private static readonly string HiddenPath = Path.GetFullPath($"{PathFinderUtility.ResolvePath<IPathFinder>(typeof(LuminPathFinder)).ToForwardSlashes()}\\{MixedRealityPreferences.HIDDEN_PROFILES_PATH}");
+
+        static LuminPackageInstaller()
+        {
+            if (!EditorPreferences.Get($"{nameof(LuminPackageInstaller)}", false))
+            {
+                EditorPreferences.Set($"{nameof(LuminPackageInstaller)}", PackageInstaller.TryInstallProfiles(HiddenPath, DefaultPath));
+            }
+        }
+    }
+}
