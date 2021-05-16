b7d8ca0bfe383c824e2b0afa978131c636833d4a
Stephen Hodgson <hodgson.designs@gmail.com>
2021-05-16 13:35:47 -0400

    updated assembly info

--- XRTK.Lumin/Packages/com.xrtk.lumin/Editor/AssemblyInfo.cs
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Editor/AssemblyInfo.cs
@@ -1,4 +1,11 @@
-﻿// Copyright (c) XRTK. All rights reserved.
+// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #define XRTK_LUMIN_EDITOR
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.2.8")]
+[assembly: AssemblyTitle("com.xrtk.lumin.editor")]
+[assembly: AssemblyCompany("XRTK")]
+[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
--- XRTK.Lumin/Packages/com.xrtk.lumin/Runtime/AssemblyInfo.cs
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Runtime/AssemblyInfo.cs
@@ -1,4 +1,11 @@
-﻿// Copyright (c) XRTK. All rights reserved.
+// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #define XRTK_LUMIN
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.2.8")]
+[assembly: AssemblyTitle("com.xrtk.lumin")]
+[assembly: AssemblyCompany("XRTK")]
+[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
