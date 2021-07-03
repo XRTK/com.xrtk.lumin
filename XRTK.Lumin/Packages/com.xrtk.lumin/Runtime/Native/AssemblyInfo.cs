81113e95b76bb47a6825de9821464e3b9bd37149
Dino Fejzagić <dino.f@live.de>
2021-07-03 21:55:50 +0200

    Fix "A using clause must precede all other elements defined in the namespace" (#116)
    Packages\com.xrtk.lumin\Runtime\Native\AssemblyInfo.cs(9,1): error CS1529: A using clause must precede all other elements defined in the namespace except extern alias declarations

--- XRTK.Lumin/Packages/com.xrtk.lumin/Runtime/Native/AssemblyInfo.cs
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Runtime/Native/AssemblyInfo.cs
@@ -1,7 +1,11 @@
-﻿// Copyright (c) XRTK. All rights reserved.
+// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Runtime.CompilerServices;
+using System.Reflection;
 
 [assembly: InternalsVisibleTo("XRTK.Lumin")]
 [assembly: InternalsVisibleTo("XRTK.Lumin.Editor")]
+
+[assembly: AssemblyVersion("0.2.10")]
+[assembly: AssemblyTitle("com.xrtk.lumin.native")]
