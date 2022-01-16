61252f2a1c5b8d1e984fc5d65d8e5e48b0bd9356
Dino Fejzagić <dino.f@live.de>
2022-01-16 13:21:51 +0100

    Update README and LuminPlatform.IsAvailable

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# Lumin
+# XRTK - Lumin Platform Module
 
-The Lumin platform components for the [XRTK - Mixed Reality Toolkit](https://github.com/XRTK/XRTK-Core)
+This package includes and enables support for the Lumin platform when using the [XRTK](https://github.com/XRTK/XRTK-Core).
 
-## Build Status
-
+[![Discord](https://img.shields.io/discord/597064584980987924.svg?label=&logo=discord&logoColor=ffffff&color=7389D8&labelColor=6A7EC2)](https://discord.gg/7DR6QJE)
 [![Build Status](https://dev.azure.com/xrtk/Mixed%20Reality%20Toolkit/_apis/build/status/com.xrtk.lumin?branchName=master)](https://dev.azure.com/xrtk/Mixed%20Reality%20Toolkit/_build/latest?definitionId=47&branchName=master)
 [![openupm](https://img.shields.io/npm/v/com.xrtk.lumin?label=openupm&registry_uri=https://package.openupm.com)](https://openupm.com/packages/com.xrtk.lumin/)
+
+## What's included?
+
+- Data Providers for XRTK's `Camera System`
+- Data Providers for XRTK's `Input System`
+- Data Providers for XRTK's `Spatial Awareness System`
+
+## Supported Devices
+
+- Magic Leap One
+
+## Requirements
+
+- [Unity 2020.3 and above](https://unity.com/)
+- [XRTK.Core](https://github.com/XRTK/com.xrtk.core)
+
+## Documentation
+
+Learn more about XRTK and how to use this package at [https://www.xrtk.io](https://www.xrtk.io) and make sure
+to join our [Discord](https://discord.gg/7DR6QJE) to find help and support.
--- XRTK.Lumin/Packages/com.xrtk.lumin/Runtime/LuminPlatform.cs
+++ XRTK.Lumin/Packages/com.xrtk.lumin/Runtime/LuminPlatform.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
 namespace XRTK.Definitions.Platforms
 {
     /// <summary>
@@ -9,27 +13,68 @@ namespace XRTK.Definitions.Platforms
     [System.Runtime.InteropServices.Guid("39EBB366-21A7-4D08-9412-036B32976D0C")]
     public class LuminPlatform : BasePlatform
     {
+        private const string luminXRDisplaySubsystemDescriptorId = "MagicLeap-Display";
+        private const string luminXRInputSubsystemDescriptorId = "MagicLeap-Input";
+
         /// <inheritdoc />
         public override bool IsAvailable
         {
             get
             {
-#if PLATFORM_LUMIN
-                return !UnityEngine.Application.isEditor;
-#else
-                return false;
-#endif
+                var displaySubsystems = new List<XRDisplaySubsystem>();
+                SubsystemManager.GetSubsystems(displaySubsystems);
+                var luminXRDisplaySubsystemDescriptorFound = false;
+
+                for (var i = 0; i < displaySubsystems.Count; i++)
+                {
+                    var displaySubsystem = displaySubsystems[i];
+                    if (displaySubsystem.SubsystemDescriptor.id.Equals(luminXRDisplaySubsystemDescriptorId) &&
+                        displaySubsystem.running)
+                    {
+                        luminXRDisplaySubsystemDescriptorFound = true;
+                    }
+                }
+
+                // The Lumin XR Display Subsystem is not available / running,
+                // Lumin as a platform doesn't seem to be available.
+                if (!luminXRDisplaySubsystemDescriptorFound)
+                {
+                    return false;
+                }
+
+                var inputSubsystems = new List<XRInputSubsystem>();
+                SubsystemManager.GetSubsystems(inputSubsystems);
+                var luminXRInputSubsystemDescriptorFound = false;
+
+                for (var i = 0; i < inputSubsystems.Count; i++)
+                {
+                    var inputSubsystem = inputSubsystems[i];
+                    if (inputSubsystem.SubsystemDescriptor.id.Equals(luminXRInputSubsystemDescriptorId) &&
+                        inputSubsystem.running)
+                    {
+                        luminXRInputSubsystemDescriptorFound = true;
+                    }
+                }
+
+                // The Lumin XR Input Subsystem is not available / running,
+                // Lumin XR as a platform doesn't seem to be available.
+                if (!luminXRInputSubsystemDescriptorFound)
+                {
+                    return false;
+                }
+
+                // Only if both, Display and Input Lumin XR Subsystems are available
+                // and running, the platform is considered available.
+                return true;
             }
         }
 
 #if UNITY_EDITOR
-
         /// <inheritdoc />
         public override UnityEditor.BuildTarget[] ValidBuildTargets { get; } =
         {
             UnityEditor.BuildTarget.Lumin
         };
-
 #endif // UNITY_EDITOR
     }
 }
